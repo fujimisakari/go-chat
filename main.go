0156c80e3258f8001737fd12d902e897de3d0bde
Fujimisakari <fujimisakari@gmail.com>
2017-03-20 15:20:48 +0900

    Add login feature

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"flag"
-	"github.com/fujimisakari/go-chat/trace"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/fujimisakari/go-chat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -22,18 +28,36 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 
+	// Gomniauthセットアップ
+	gomniauth.SetSecurityKey("fizzbuzz")
+	gomniauth.WithProviders(
+		facebook.New(facebookClientID, facebookSecret, facebookRedirectURL),
+		github.New(githubClientID, githubSecret, githubRedirectURL),
+		google.New(googleClientID, googleSecret, googleRedirectURL),
+	)
+
 	// チャットルームを開始します
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", logoutHandler)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	go r.run()
 
